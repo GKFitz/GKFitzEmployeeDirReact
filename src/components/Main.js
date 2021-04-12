24219a6f9c17d0f6363eb5f19b0c680d497e56e9
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-12 16:36:37 -0400

    set up the Utils API.js and the main. Began pulling from the Api

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import API from '../utils/API.js';
+
+   
+
+export default class Main extends Component {
+  
+    render()
+
+    return (
+
+        <div>
+
+            Main
+         
+           
+        </div>
+    
+    )
+
+}
