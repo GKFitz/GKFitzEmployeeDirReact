a9ccab846134e17f23db581b09a8485462ac2a26
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-12 17:26:07 -0400

    setting up employee row component

--- src/components/EmployeeRow.js
+++ src/components/EmployeeRow.js
@@ -1,5 +1,20 @@
 import React from 'react'
 
 export default function EmployeeRow(props) {
-    return ()
+    return (
+        <tr>
+            <td>
+                {props.firstName}
+            </td>
+            <td>
+                {props.lastName}
+
+            </td>
+            <td>
+                {props.email}
+            </td>
+            
+
+        </tr>
+    )
 }
