164291db971ac6f5d8ceffed53ae809839df09ed
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-13 02:21:33 -0400

    styled the search bar, with better spacing

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ import React from 'react';
    
 function SearchBar(props) {
     return (
-    <input type="text" value= {props.nameState} onChange= {props.handleInputChange} placeholder="Search.."></input>
+    <input type="text" class= "searchBar" value= {props.nameState} onChange= {props.handleInputChange} placeholder="Search.."></input>
 
     )
 
